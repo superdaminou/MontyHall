9910a4be424fa4b92ed7bfaabc81b23d437f88ae
damien <dami13795@msn.com>
2017-11-16 11:16:55 +0100

    impl Retour

--- app/src/main/java/com/example/damien/montyhall/Resultat.java
+++ app/src/main/java/com/example/damien/montyhall/Resultat.java
@@ -1,12 +1,15 @@
 package com.example.damien.montyhall;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Resultat extends AppCompatActivity {
 
-
+    Button retour;
     TextView res;
 
     @Override
@@ -15,7 +18,14 @@ public class Resultat extends AppCompatActivity {
         setContentView(R.layout.activity_resultat);
 
         res = (TextView) findViewById(R.id.Res);
+        retour = (Button) findViewById(R.id.retour);
 
+        retour.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retourMenu();
+            }
+        });
         Bundle extra = getIntent().getExtras();
         String choix = extra.getString("choix");
         if("voiture".equals(choix)){
@@ -23,5 +33,11 @@ public class Resultat extends AppCompatActivity {
         }else{
             res.setText("Perdu");
         }
+    }
+
+
+    public void retourMenu(){
+        Intent retour = new Intent(this,Acceuil.class);
+        startActivity(retour);
     }
 }
